09b954c57020876de7b38ad483027f1d743bce83
viktork2026 <ondewhfe@yandex.kz>
2026-06-26 03:39:55 +0500

    feat: implement NetworkMovieLoader

---
+++ MovieQuiz/Services/NetworkMovieLoader.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkMovieLoader.swift
+//  MovieQuiz
+//
+//  Created by Viktor Kim on 26.06.2026.
+//
+
+import Foundation
+
+protocol MovieLoader {
+    func loadMovies(
+        handler: @escaping (Result<MostPopularMovies, Error>) -> Void
+    )
+}
+
+struct NetworkMovieLoader: MovieLoader {
+    private let networkClient: NetworkClient = .init()
+    private let mostPopularMoviesUrl: URL = .init(
+        staticString: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf"
+    )
+
+    func loadMovies(
+        handler: @escaping (Result<MostPopularMovies, Error>) -> Void
+    ) {
+        networkClient.fetch(url: mostPopularMoviesUrl) { result in
+            let result = result.flatMap { data in
+                Result {
+                    try JSONDecoder().decode(MostPopularMovies.self, from: data)
+                }
+            }
+
+            handler(result)
+        }
+    }
+}

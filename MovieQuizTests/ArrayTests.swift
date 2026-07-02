9fab672643eb3cc14a40b94af0d5d8df4ae2c98f
viktork2026 <ondewhfe@yandex.kz>
2026-07-03 03:49:35 +0500

    test: add unit tests for safe array subscript extension

---
+++ MovieQuizTests/ArrayTests.swift
@@ -0,0 +1,28 @@
+//
+//  ArrayTests.swift
+//  ArrayTests
+//
+//  Created by Viktor Kim on 03.07.2026.
+//
+
+import XCTest
+@testable import MovieQuiz
+
+final class ArrayTests: XCTestCase {
+    func testGetValueInRange() throws {
+        let array = [0, 1, 2, 3]
+        
+        let value = array[safe: 2]
+        
+        XCTAssertNotNil(value)
+        XCTAssertEqual(value, 2)
+    }
+    
+    func testGetValueOutOfRange() throws {
+        let array = [0, 1, 2, 3]
+        
+        let value = array[safe: 20]
+        
+        XCTAssertNil(value)
+    }
+}

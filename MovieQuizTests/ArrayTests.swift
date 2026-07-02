a50ab8951f6ef60158c26d5736ccabd8d2d43417
viktork2026 <ondewhfe@yandex.kz>
2026-07-03 04:43:34 +0500

    style: add missing newline in tests

--- MovieQuizTests/ArrayTests.swift
+++ MovieQuizTests/ArrayTests.swift
@@ -6,23 +6,24 @@
 //
 
 import XCTest
+
 @testable import MovieQuiz
 
 final class ArrayTests: XCTestCase {
     func testGetValueInRange() throws {
         let array = [0, 1, 2, 3]
-        
+
         let value = array[safe: 2]
-        
+
         XCTAssertNotNil(value)
         XCTAssertEqual(value, 2)
     }
-    
+
     func testGetValueOutOfRange() throws {
         let array = [0, 1, 2, 3]
-        
+
         let value = array[safe: 20]
-        
+
         XCTAssertNil(value)
     }
 }
